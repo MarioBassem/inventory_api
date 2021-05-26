16d13cd5b10ce572e44257608e1a5eed96ee0815
mariobassem <mariobassem12@gmail.com>
2021-05-26 20:50:09 +0200

    implementing auth...

--- auth/auth.js
+++ auth/auth.js
@@ -9,30 +9,42 @@ module.exports = async function(req, res, next){
             throw new Error('Authorizatino header missing!');
         }
 
-        const parts = header.split(' ');
+        const payload = await get_payload(header);
+
+        // const permissions = 
         
-        if(parts[0].toLowerCase() != 'bearer'){
-            throw new Error('Authorization header must start with "Bearer".');
-        }
-
-        if(parts.length == 1){
-            throw new Error('Token not found!');
-        }
-
-        if(parts.length > 2){
-            throw new Error('Authorization header must be bearer token');
-        }
-
-        const token = parts[1];
-
-        const decoded = await jwt.verify(
-            token,
-            process.env.TOKEN_SECRET
-        );
-        
-        console.log(decoded);
+        await check_permissions(payload, permissions);
+        console.log(payload);
         next();
     }catch(err){
         console.log('Error: ' + err + '\n');
     }
 }
+
+async function get_payload(header){
+    const parts = header.split(' ');
+        
+    if(parts[0].toLowerCase() != 'bearer'){
+        throw new Error('Authorization header must start with "Bearer".');
+    }
+
+    if(parts.length == 1){
+        throw new Error('Token not found!');
+    }
+
+    if(parts.length > 2){
+        throw new Error('Authorization header must be bearer token');
+    }
+
+    const token = parts[1];
+
+    return await jwt.verify(
+        token,
+        process.env.TOKEN_SECRET
+    );
+}
+
+async function check_permissions(payload, permissions){
+    return true;
+}
+
